f9060c04b766b8e3004a89bdde3fd2cebf29d9e8
User <User@DESKTOP-0SGD7C0.powerhub>
2025-09-22 06:14:19 +0100

    22Sep

---
+++ samplemaven/src/main/java/exceptionHandling/exceptionEg.java
@@ -0,0 +1,13 @@
+package exceptionHandling;
+
+public class exceptionEg {
+
+	
+	public static void main(String[] args) {
+		int a =10, b=0;
+		int c=a/b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ samplemaven/src/main/java/exceptionHandling/nullPointer.java
@@ -0,0 +1,12 @@
+package exceptionHandling;
+
+public class nullPointer {
+
+	public static void main(String[] args) {
+		String s= null;
+		s.length();
+		System.out.println(s);
+
+	}
+
+}
